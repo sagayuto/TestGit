b0ea4f4c5d30bdde18197514ba87d90a56f68e0f
佐賀祐人 <sagayuuto@yuto.local>
2019-07-11 19:44:55 +0900

    first commit

---
+++ Pods/Target Support Files/Pods-test60UITests/Pods-test60UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_test60UITests : NSObject
+@end
+@implementation PodsDummy_Pods_test60UITests
+@end
---
+++ test60/TopViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TopViewController.swift
+//  test60
+//
+//  Created by 佐賀祐人 on 2019/07/11.
+//  Copyright © 2019 net.sample. All rights reserved.
+//
+
+import UIKit
+
+class TopViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
